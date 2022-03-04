9f36f6d1e00f1af5a3624114df6ea53ea220a08f
YoussouphNdiour <83768182+YoussouphNdiour@users.noreply.github.com>
2022-03-04 00:17:25 +0000

    update

--- config/database.js
+++ config/database.js
@@ -8,10 +8,17 @@ require('dotenv').config();
 //     "database": "nigger84_bmgmysql2022",
 //     "port": "8889"
 // });
+// module.exports.DATABASE = mysql.createPool({
+//     "host": "mysql1007.mochahost.com",
+//     "user": "nigger84_bmg2022",
+//     "password": "QZf7b)PMe@;6",
+//     "database": "nigger84_bmgmysql2022",
+//     "port": "3306"
+// });
 module.exports.DATABASE = mysql.createPool({
-    "host": "mysql1007.mochahost.com",
-    "user": "nigger84_bmg2022",
-    "password": "QZf7b)PMe@;6",
-    "database": "nigger84_bmgmysql2022",
+    "host": "us-cdbr-east-05.cleardb.net",
+    "user": "bf67e6da4810eb",
+    "password": "44fc425d",
+    "database": "heroku_195f897dd741e64	",
     "port": "3306"
 });
